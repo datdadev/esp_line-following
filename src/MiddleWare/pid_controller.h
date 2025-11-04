e4483b1b9c1bf1b0b6f094aaccb7d1d7db2116b4
datdadev <112007525+datdadev@users.noreply.github.com>
2025-11-04 11:19:42 +0700

    Remove uppercase directory files due to case sensitivity issue

--- src/MiddleWare/pid_controller.h
+++
@@ -1,9 +0,0 @@
-#ifndef PID_CONTROLLER_H
-#define PID_CONTROLLER_H
-
-#include <stdint.h>
-
-// ===================== PID CONTROLLER =====================
-float PID(float e);
-
-#endif // PID_CONTROLLER_H
